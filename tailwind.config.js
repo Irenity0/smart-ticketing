28fa09663dc7f0d07f7fe8f8e1242afc0d3faa69
Irenity <lilithkard77@gmail.com>
2024-09-21 21:21:36 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+    	extend: {
+			color: {
+				primary: '#1dd100',
+				secondary: '#030712',
+				couponOne: '#ffbf0f',
+				couponTwo: '#f78c9c',
+				mobileMenu: '#f7f8f8',
+				body: '#03071299',
+				buttonColor: 'rgba(29, 209, 0, 0.15)'
+			},
+			fontFamily: {
+				Raleway: 'Raleway',
+				Inter: 'Inter'
+			}
+    	},
+    },
+    plugins: [],
+  }
